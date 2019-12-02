46733e7b70e7851f3cee63b437ac87301f6ecf00
SHOBHIT <shobhitsaini95@gmail.com>
2019-12-02 23:59:25 +0530

    Update Wave_Array.cpp

--- Day4/Wave_Array.cpp
+++ Day4/Wave_Array.cpp
@@ -9,3 +9,23 @@ Given [1, 2, 3, 4]
 One possible answer : [2, 1, 4, 3]
 Another possible answer : [4, 1, 3, 2]
 */
+vector<int> Solution::wave(vector<int> &A) {
+    sort(A.begin(), A.end());
+    int i, temp, size;
+    if(A.size()%2 == 0)
+    {
+        size = A.size();
+    }
+    else
+    {
+        size = A.size()-1;
+    }
+    for(i = 0; i < size; i+= 2)
+    {
+        temp = A[i];
+        A[i] = A[i + 1];
+        A[i + 1] = temp;
+        
+    }
+    return A;
+}
