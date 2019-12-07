419820b15fd934e243f852702a5c520a08a1e113
SHOBHIT <shobhitsaini95@gmail.com>
2019-12-07 19:12:03 +0530

    Create Set Matrix Zeros.cpp

---
+++ Day8/Set Matrix Zeros.cpp
@@ -0,0 +1,76 @@
+/*
+Given a matrix, A of size M x N of 0s and 1s. If an element is 0, set its entire row and column to 0.
+
+Note: This will be evaluated on the extra memory used. Try to minimize the space and time complexity.
+
+
+
+Input Format:
+
+The first and the only argument of input contains a 2-d integer matrix, A, of size M x N.
+Output Format:
+
+Return a 2-d matrix that satisfies the given conditions.
+Constraints:
+
+1 <= N, M <= 1000
+0 <= A[i][j] <= 1
+Examples:
+
+Input 1:
+    [   [1, 0, 1],
+        [1, 1, 1], 
+        [1, 1, 1]   ]
+
+Output 1:
+    [   [0, 0, 0],
+        [1, 0, 1],
+        [1, 0, 1]   ]
+
+Input 2:
+    [   [1, 0, 1],
+        [1, 1, 1],
+        [1, 0, 1]   ]
+
+Output 2:
+    [   [0, 0, 0],
+        [1, 0, 1],
+        [0, 0, 0]   ]
+*/
+void Solution::setZeroes(vector<vector<int> > &A) {
+    vector<int> B;
+    int i, j, k;
+    for(i = 0; i < A.size(); i++)
+    {
+        for(j = 0; j < A[i].size(); j++)
+        {
+            if(A[i][j] == 0)
+            {
+                for(k = 0; k < A[i].size(); k++)
+                {
+                    if(A[i][k] != 0)
+                    {
+                        A[i][k] = -1;
+                    }
+                }
+                for(k = 0; k < A.size(); k++)
+                {
+                    if(A[k][j] != 0)
+                    {
+                        A[k][j] = -1;
+                    }
+                }
+            }
+        }
+    }
+    for(i = 0; i < A.size(); i++)
+    {
+        for(j = 0; j < A[i].size(); j++)
+        {
+            if(A[i][j] == -1)
+            {
+                A[i][j] = 0;
+            }
+        }
+    }
+}
