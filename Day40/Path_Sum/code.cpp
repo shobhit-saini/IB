4cfa555a395e1c00043b14a09097cfa6f9508018
SHOBHIT <shobhitsaini95@gmail.com>
2020-01-29 23:18:28 +0530

    Create code.cpp

---
+++ Day40/Path_Sum/code.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::hasPathSum(TreeNode* A, int B) {
+    if(A==NULL)
+        return 0;
+    else 
+    {
+        if(B - A->val == 0 && A->left == NULL && A->right == NULL)
+            return 1;
+        return (hasPathSum(A->left, B-A->val) || hasPathSum(A->right, B-A->val));
+    }
+}
