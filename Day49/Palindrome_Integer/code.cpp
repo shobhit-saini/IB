02b97950d9fc9d1ad00347518d5d329eb7ee4c98
SHOBHIT <shobhitsaini95@gmail.com>
2020-02-07 08:44:44 +0530

    Create code.cpp

---
+++ Day49/Palindrome_Integer/code.cpp
@@ -0,0 +1,28 @@
+int Solution::isPalindrome(int A) {
+    int temp = A, count = 0;
+    if(A /10 <= 0)
+    {
+        return 1;
+    }
+    while(temp != 0)
+    {
+        temp = temp/10;
+        count++;
+    }
+    int i = count-1;
+    int j = 1;
+    temp = A;
+    while(i != j)
+    {
+        
+        int var = pow(10,1);
+        int left = (A/pow(10,i));
+        left = left%10;
+        int right = temp%var;
+        if(left != right)
+            return 0;
+        i--; j++;temp = temp/10;
+    }
+    return 1;
+    
+}
