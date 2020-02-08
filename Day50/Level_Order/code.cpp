554dc9e8eb6ad687e3bd8ad94609f5863a62a1fc
SHOBHIT <shobhitsaini95@gmail.com>
2020-02-08 08:49:42 +0530

    Create code.cpp

---
+++ Day50/Level_Order/code.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int size(TreeNode* A)
+{
+    if(A == NULL)
+        return 0;
+    int hl = size(A->left);
+    int hr = size(A->right);
+    if(hl < hr)
+    {
+        return hr+1;
+    }
+    else
+    {
+        return hl+1;
+    }
+}
+vector<vector<int> > Solution::levelOrder(TreeNode* A) {
+    int a = size(A);
+    vector<vector<int>> result(a);
+    if(A == NULL)
+        return result;
+    queue<TreeNode*> q;
+    q.push(A);
+    TreeNode* ptr = A, *x;
+    int count1 = 1, count2 = 1, i = 0, j;
+    while(count2 != 0)
+    {
+        count2 = 0;
+        while(count1 != 0)
+        {
+            count1--;
+            ptr = q.front();
+            q.pop();
+            result[i].push_back(ptr->val);
+            if(ptr->left != NULL)
+            {
+                q.push(ptr->left);
+                count2++;
+            }
+            if(ptr->right != NULL)
+            {
+                q.push(ptr->right);
+                count2++;
+            }
+        }
+        i++;
+        count1 = count2;
+    }
+    return result;
+}
