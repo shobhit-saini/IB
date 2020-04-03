21263ce9ca2d8af51b27e12a8fbdc227bf3a6ba5
SHOBHIT <shobhitsaini95@gmail.com>
2020-04-03 19:35:13 +0530

    Create Best_Time_to_Buy_and_Sell_Stocks_I.cpp

---
+++ DP/Best_Time_to_Buy_and_Sell_Stocks_I.cpp
@@ -0,0 +1,39 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+*/
+int Solution::maxProfit(const vector<int> &prices) {
+     if(prices.size() == 0)
+            return 0;
+        int i, j, size = prices.size(), min_pointer = 0, val, profit = 0;
+        for( i = 1; i < size; i++ )
+        {
+            if( prices[min_pointer] > prices[i] )
+            {
+            
+                min_pointer = i;    
+            }
+            else
+            {
+                val = prices[i] - prices[min_pointer];
+                if(val > profit)
+                    profit = val;
+            }
+        }
+        return profit;
+}
